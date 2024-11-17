8c5962f7323831c11e5ec6ff83101d27c8e99751
Andinibus <andinibus@gmail.com>
2024-11-17 22:06:07 +0100

    Layout überarbeitet

--- Script/translations.js
+++
@@ -1,8 +0,0 @@
-/* Englisch
-
-    The aim of this report is to show how the knowledge of the Traffic laws gets lost after passig the drinving test
-    As can be seen the knowledge of the trffic laws extremly decreases after passing the drinving test
-    To improve the current situation we could do some tests after the drinving test or we could decres the stuff we ned to learn so we remember more because its not that much anymore
-    All in all, we dont know that much after the driving test as we knew bevore so its useless in general.
-
-*/
