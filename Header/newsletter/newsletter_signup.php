076a47fa6d0c40d04a1d3e9b9589b4f5aed04bb3
cheatkurtl <leo.maier2008@gmail.com>
2024-11-26 18:59:09 +0100

    php

---
+++ Header/newsletter/newsletter_signup.php
@@ -0,0 +1,25 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // 1. E-Mail-Adresse aus POST-Daten lesen
+    $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+
+    // 2. Validierung
+    if (!$email) {
+        echo "Ungültige E-Mail-Adresse.";
+        exit;
+    }
+
+    // 3. Datei zur Speicherung (oder später Datenbank verwenden)
+    $file = 'newsletter_emails.txt';
+
+    // 4. E-Mail speichern
+    file_put_contents($file, $email . PHP_EOL, FILE_APPEND | LOCK_EX);
+
+    mail($email, "Bestätigung Newsletter", "Danke für deine Anmeldung zu unserem Newsletter!", "From: no-reply@peckventure.com");
+
+    // 5. Erfolgsmeldung
+    echo "Danke, dass du dich für unseren Newsletter angemeldet hast!";
+} else {
+    echo "Ungültige Anfrage.";
+}
+?>
---
+++ Header/newsletter/readme.md
@@ -0,0 +1,14 @@
+3. Webserver einrichten
+Lokales Testen mit XAMPP oder WAMP:
+
+Installiere XAMPP oder WAMP.
+Platziere deine Dateien in den Ordner htdocs (XAMPP) oder www (WAMP).
+Starte Apache und öffne http://localhost/newsletter.html.
+Live-Webserver:
+
+Lade die Dateien auf deinen Server, z. B. mit FTP.
+Stelle sicher, dass PHP aktiviert ist.
+4. Datei zur Speicherung von E-Mails
+Der obige PHP-Code speichert die E-Mails in einer Textdatei newsletter_emails.txt. Stelle sicher:
+
+Die Datei befindet sich auf dem Server und hat Schreibrechte.
